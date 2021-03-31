701d80fee37229fdcc4bc8f0990dd043fd69a0f7
eduardo reyes <reyes.eduardo21@att.net>
2021-03-31 16:20:49 -0500

    initial_setup

---
+++ PizzaBox/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Run();
+        }
+
+        private static void Run()
+        {
+            Console.WriteLine("Welcome to PizzaBox ");
+            var stores = new List<string>()
+            {
+                "Store 001",
+                "Store 002",
+                "Store 003"
+            };
+
+            foreach (var item in stores)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+
+}
